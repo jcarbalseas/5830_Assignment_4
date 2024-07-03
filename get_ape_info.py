434d909a32b7d61bdf849df32b56cef42879ed53
jcarbalseas <49366476+jcarbalseas@users.noreply.github.com>
2024-07-03 09:10:57 -0700

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -21,6 +21,8 @@ api_url = #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
+contract = web3.eth.contract(address=contract_address, abi=abi)
+
 def get_ape_info(apeID):
 	assert isinstance(apeID,int), f"{apeID} is not an int"
 	assert 1 <= apeID, f"{apeID} must be at least 1"
@@ -28,6 +30,32 @@ def get_ape_info(apeID):
 	data = {'owner': "", 'image': "", 'eyes': "" }
 	
 	#YOUR CODE HERE	
+
+	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
+	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
+	return data
+
+	#YOUR CODE HERE	
+  	owner = contract.functions.ownerOf(apeID).call()
+  	data['owner'] = owner
+
+  	token_uri = contract.functions.tokenURI(apeID).call()
+
+  	if token_uri.startswith("ipfs://"):
+    		ipfs_hash = token_uri.split("ipfs://")[1]
+    		ipfs_url = f"https://ipfs.io/ipfs/{ipfs_hash}"
+  	else:
+    		ipfs_url = token_uri
+
+  	response = requests.get(ipfs_url)
+  	metadata = response.json()
+
+  	data['image'] = metadata.get('image', "")
+  	attributes = metadata.get('attributes', [])
+  	for attribute in attributes:
+    	if attribute['trait_type'] == 'Eyes':
+      		data['eyes'] = attribute['value']
+      		break
 
 	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
 	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"

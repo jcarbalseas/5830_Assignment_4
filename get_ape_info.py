7fa0606cee978dc5a933b02cf5650dc0eb8acdc4
jcarbalseas <49366476+jcarbalseas@users.noreply.github.com>
2024-07-06 00:08:34 -0700

    Update get_ape_info.py

--- get_ape_info.py
+++ get_ape_info.py
@@ -1,3 +1,62 @@
+# from web3 import Web3
+# from web3.contract import Contract
+# from web3.providers.rpc import HTTPProvider
+# import requests
+# import json
+# import time
+
+# bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+# contract_address = Web3.toChecksumAddress(bayc_address)
+
+# #You will need the ABI to connect to the contract
+# #The file 'abi.json' has the ABI for the bored ape contract
+# #In general, you can get contract ABIs from etherscan
+# #https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+# with open('/home/codio/workspace/abi.json', 'r') as f:
+# 	abi = json.load(f) 
+
+# ############################
+# #Connect to an Ethereum node
+# api_url = "https://mainnet.infura.io/v3/bfae8555ac6f497097223d4573f9c441" #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
+# provider = HTTPProvider(api_url)
+# web3 = Web3(provider)
+# apeID = '10'
+
+# contract = web3.eth.contract(address=contract_address, abi=abi)
+
+# def get_ape_info(apeID):
+# 	assert isinstance(apeID,int), f"{apeID} is not an int"
+# 	assert 1 <= apeID, f"{apeID} must be at least 1"
+
+# 	data = {'owner': "", 'image': "", 'eyes': "" }
+
+# 	#YOUR CODE HERE 
+# 	owner = contract.functions.ownerOf(apeID).call() 
+# 	data['owner'] = owner 
+
+# 	token_uri = contract.functions.tokenURI(apeID).call()
+
+
+# 	if token_uri.startswith("ipfs://"):
+# 		ipfs_hash = token_uri.split("ipfs://")[1]
+# 		ipfs_url = f"https://ipfs.io/ipfs/{ipfs_hash}"
+# 	else:
+# 		ipfs_url = token_uri
+
+# 	response = requests.get(ipfs_url)
+# 	metadata = response.json()
+
+# 	data['image'] = metadata.get('image', "")
+# 	attributes = metadata.get('attributes', [])
+# 	for attribute in attributes:
+# 		if attribute['trait_type'] == 'Eyes':
+# 			data['eyes'] = attribute['value']
+# 			break
+
+# 	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
+# 	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
+# 	return data
+
 from web3 import Web3
 from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
@@ -6,7 +65,16 @@ import json
 import time
 
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+
+############################
+#Connect to an Ethereum node
+api_url = "https://mainnet.infura.io/v3/bfae8555ac6f497097223d4573f9c441" #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
+provider = HTTPProvider(api_url)
+web3 = Web3(provider)
+
+
 contract_address = Web3.toChecksumAddress(bayc_address)
+
 
 #You will need the ABI to connect to the contract
 #The file 'abi.json' has the ABI for the bored ape contract
@@ -15,14 +83,11 @@ contract_address = Web3.toChecksumAddress(bayc_address)
 with open('/home/codio/workspace/abi.json', 'r') as f:
 	abi = json.load(f) 
 
-############################
-#Connect to an Ethereum node
-api_url = "https://mainnet.infura.io/v3/bfae8555ac6f497097223d4573f9c441" #YOU WILL NEED TO TO PROVIDE THE URL OF AN ETHEREUM NODE
-provider = HTTPProvider(api_url)
-web3 = Web3(provider)
-apeID = '10'
+
+
 
 contract = web3.eth.contract(address=contract_address, abi=abi)
+apeID = '10'
 
 def get_ape_info(apeID):
 	assert isinstance(apeID,int), f"{apeID} is not an int"
@@ -35,7 +100,7 @@ def get_ape_info(apeID):
 	data['owner'] = owner 
 
 	token_uri = contract.functions.tokenURI(apeID).call()
-
+##### NEEDSWORKONSPACES ############
 
 	if token_uri.startswith("ipfs://"):
 		ipfs_hash = token_uri.split("ipfs://")[1]
